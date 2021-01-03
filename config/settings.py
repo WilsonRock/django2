b1b906e2424033d7ec400fb76841ae4a1930ca74
Norbey <43182948+WilsonRock@users.noreply.github.com>
2021-01-03 15:43:05 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -86,12 +86,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
-import dj_database_url
-from decouple import config
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
